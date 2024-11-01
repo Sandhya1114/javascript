c981032768252b53e66dcbb9d23265ae1b67f086
Sandhya1114 <146408600+Sandhya1114@users.noreply.github.com>
2024-11-01 09:38:56 +0000

    container config

---
+++ BasicJS/02_variable.js
@@ -0,0 +1,11 @@
+const name="sandhya";
+var acc=12223;
+let course="b.tech";
+//name="sanu";//once is declare can not change
+console.log(name);
+//var should never be used because of scop of function
+//always use let for variable declare
+num=34;
+let sum;
+console.table([name,course,acc,num,sum]);
+console.log(typeof(num));
